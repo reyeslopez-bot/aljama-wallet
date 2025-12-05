bcc78c0cfa7bff216a231076e314156b0525772a
Rafael <reyeslopez@mail.tau.ac.il>
2025-12-05 16:18:52 +0200

    updated prisma and tsconfig files

--- infra/db/prisma-crdb.ts
+++ infra/db/prisma-crdb.ts
@@ -1,2 +1,19 @@
 // infra/db/prisma-crdb.ts
 import { PrismaClient as PrismaClientCrdb } from '@/prisma/generated/prisma-crdb'
+
+const globalForCrdb = globalThis as unknown as {
+  prismaCrdb?: PrismaClientCrdb
+}
+
+export const prismaCrdb =
+  globalForCrdb.prismaCrdb ??
+  new PrismaClientCrdb({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForCrdb.prismaCrdb = prismaCrdb
+}
--- infra/db/prisma-pg.ts
+++ infra/db/prisma-pg.ts
@@ -1,2 +1,19 @@
 // infra/db/prisma-pg.ts
-import { PrismaClient as PrismaClientPg } from '@/prisma/generated/prisma-pg'
+import { PrismaClient as PrismaClientPg } from '@/prisma/generated/pg'
+
+const globalForPg = globalThis as unknown as {
+  prismaPg?: PrismaClientPg
+}
+
+export const prismaPg =
+  globalForPg.prismaPg ??
+  new PrismaClientPg({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPg.prismaPg = prismaPg
+}
