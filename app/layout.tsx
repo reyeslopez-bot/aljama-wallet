53fb0b875dfdf144e4dc022601cfeed271534b40
Rafael RL <143941516+reyeslopez-bot@users.noreply.github.com>
2026-01-15 17:40:00 +0200

    Fix global styles and refine navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Providers from './Providers.client'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col antialiased text-white">
+      <body className="min-h-screen flex flex-col antialiased">
         <Providers>{children}</Providers>
       </body>
     </html>
