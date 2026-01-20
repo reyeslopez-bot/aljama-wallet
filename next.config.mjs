6dde442f84b5545507a50c71e45eef5f17825e17
Rafael <reyeslopez@mail.tau.ac.il>
2026-01-20 23:36:12 +0200

    Refactor webpack configuration and update dependencies; remove unused next.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,40 @@
 // next.config.mjs
+
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
+const webpack = require("webpack");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        pino: require.resolve("pino/browser"),
-        "thread-stream": false,
-        tap: false,
-        tape: false,
-      };
-    }
+  webpack: (config) => {
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      pino: require.resolve("pino/browser"),
+      "thread-stream": false,
+      tap: false,
+      tape: false,
+      "pino-pretty": false,
+      "@react-native-async-storage/async-storage": false,
+    };
+
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp:
+          /^pino-pretty$|^@react-native-async-storage\/async-storage$/,
+      }),
+    );
+
+    config.externals = [
+      ...(config.externals || []),
+      {
+        "pino-pretty": "commonjs pino-pretty",
+        "@react-native-async-storage/async-storage":
+          "commonjs @react-native-async-storage/async-storage",
+      },
+    ];
+
     return config;
   },
 };
